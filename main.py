669885ec645c1e92a8d0a5b6dad29f41e36afb05
jenildesai25 <jenil.desai25@gmail.com>
2019-02-23 13:36:21 -0600

    added my files

--- main.py
+++ main.py
@@ -1,196 +1,207 @@
-# Jinesh Modi - 2464
-# CS-6331-001
-# Assignment3
-
-from flask import Flask, render_template, request
-from datetime import datetime
-from json import loads, dumps
-import pypyodbc
-# import pandas as pd
+import csv
+import os
+import hashlib
 import random
 import time
-import redis
-import hashlib
+from flask import Flask, render_template, request
+from db import Database, RedisCache
+from datetime import datetime
+from json import loads, dumps
 
-# from werkzeug.utils import secure_filename
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(__name__, static_url_path='', template_folder='static')
-
-server = 'jinesh-assignment3.database.windows.net'
-database = 'cloud'
-username = 'jinesh'
-password = 'J!n!@3594'
-driver = '{ODBC Driver 13 for SQL Server}'
-# cnxn = pypyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-
-myHostname = "jinesh-cloud.redis.cache.windows.net"
-myPassword = "6ZYqOPOHM465LtMEZpES5ctQkEU7SNzMOBCM6qKPpHw="
-
-r = redis.StrictRedis(host=myHostname, port=6380, password=myPassword, ssl=True)
+# Connect to db
+database = Database()
+redis = RedisCache().connection
 
 
 @app.route('/')
 def hello_world():
-    # conn = pypyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    # lstDictionaryData = []
-    # cursor = conn.cursor()
-    # startTime = time.time()
-    # query = "SELECT TOP 10 time, latitude, longitude, mag, place FROM earthquake_new"
-    # # print(query)
-    # cursor.execute(query)
-    # # print(tmp)
-    # endTime = time.time()
-    # row = cursor.fetchone()
-    # while row:
-    #     lstDictionaryData.append(row)
-    #     # print("hi!" + str(row))
-    #     row = cursor.fetchone()
-    # # return "hello!!"
-    # conn.close()
-    # executionTime = (endTime - startTime) * 1000
-    # return render_template('index.html', tableData=lstDictionaryData, tableDataLen=lstDictionaryData.__len__(), executionTime=executionTime)
-    return "Hello World from Jinesh"
+    # return 'Hello, World!'
+
+    return render_template('index.html', earthquakes=[])
 
 
-@app.route('/createtable')
-def createTable():
-    lstDictionaryData = []
-    conn = pypyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    cursor = conn.cursor()
-    # query = "CREATE TABLE dbo.all_month (\"time\" datetime, \"latitude\" FLOAT, \"longitude\" FLOAT, \"depth\" FLOAT, \"mag\" FLOAT, \"magType\" TEXT, \"nst\" INT, \"gap\" INT, \"dmin\" FLOAT, \"rms\" FLOAT, \"net\" TEXT, \"id\" TEXT, \"updated\" datetime, \"place\" TEXT, \"type\" TEXT, \"horontalError\" FLOAT, \"depthError\" FLOAT, \"magError\" FLOAT, \"magNst\" INT, \"status\" TEXT, \"locationSource\" TEXT, \"magSource\" TEXT)"
-    query = "CREATE TABLE cloudsqldb.dbo.all_month(time DATETIME,latitude FLOAT,longitude FLOAT,depth FLOAT,mag FLOAT,magType TEXT,nst INT,gap INT,dmin FLOAT,rms FLOAT,net TEXT,id TEXT,updated DATETIME,place TEXT,type TEXT,horontalError FLOAT,depthError FLOAT,magError FLOAT,magNst INT,status TEXT,locationSource TEXT,magSource TEXT)"
-    # print(query)
-    startTime = time.time()
-    # cursor.execute(query)
-    cursor.execdirect(query)
-    cursor.execdirect("CREATE INDEX all_month_mag__index ON cloudsqldb.dbo.earthquake (mag)")
-    cursor.execdirect("CREATE INDEX all_month_lat__index ON cloudsqldb.dbo.earthquake (latitude)")
-    cursor.execdirect("CREATE INDEX all_month_long__index ON cloudsqldb.dbo.earthquake (longitude)")
-    endTime = time.time()
-    conn.close()
-    executionTime = (endTime - startTime) * 1000
-    return render_template('index.html', tableData=lstDictionaryData, tableDataLen=lstDictionaryData.__len__(), executionTime=executionTime)
+@app.route('/analyze_randomq')
+def analyze_randomq():
+    cnt = int(request.args.get('count', 0))
+    source = request.args.get('source', 'sqldb')
+    random_list = [round(random.uniform(0, 10), 2) for i in range(cnt)]
 
-
-@app.route('/randomqueries')
-def randomQueries():
-    noOfQueries = int(request.args.get('queries', ''))
-    withCache = int(request.args.get('withCache', ''))
-    magnitudeStart = float(request.args.get('magnitudeStart', ''))
-    magnitudeEnd = float(request.args.get('magnitudeEnd', ''))
-
-    lstDictionaryData = []
-    lstDictionaryDataDisplay = []
-
-    conn = pypyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    cursor = conn.cursor()
-    totalExecutionTime = 0
     columns = ['time', 'latitude', 'longitude', 'place', 'mag']
+    columns_str = '"' + '","'.join(columns) + '"'
 
-    # without cache
-    if withCache == 0:
-        # print("hi!")
+    sqlquery = 'select {columns_str} from dbo.all_month where mag={mag};'
+    cursor = database.connection.cursor()
+    t = time.time()
+    if source == 'cache':
+        source_used = 'Redis Cache'
+        for mag in random_list:
+            formatted_query = sqlquery.format(columns_str=columns_str, mag=mag)
+            query_hash = hashlib.sha256(formatted_query.encode()).hexdigest()
+            result = redis.get(query_hash)
+            if result:
+                pass
+            else:
+                cursor.execute(formatted_query)
+                rows = cursor.fetchall()
 
-        magnitude_value = round(random.uniform(magnitudeStart, magnitudeEnd), 1)
-        startTime = time.time()
-        query = "SELECT locationSource FROM earthquake_new WHERE mag = '" + str(magnitude_value) + "'"
-        cursor.execute(query)
-        endTime = time.time()
-        # print(query)
-        lstDictionaryDataDisplay = cursor.fetchall()
-        # print(lstDictionaryDataDisplay)
-        executionTime = (endTime - startTime) * 1000
-        firstExecutionTime = executionTime
+                # if rows:
+                #     print('Values present for: ',query_hash)
 
-        for i in range(noOfQueries - 1):
-            totalExecutionTime = totalExecutionTime + executionTime
-            magnitude_value = round(random.uniform(magnitudeStart, magnitudeEnd), 1)
-            startTime = time.time()
-            query = "SELECT locationSource FROM earthquake_new WHERE mag = '" + str(magnitude_value) + "'"
-            cursor.execute(query)
-            endTime = time.time()
-            lstDictionaryData = list(cursor.fetchall())
-            # print("inside if")
-            # print(lstDictionaryData)
-
-            memData = []
-            for row in lstDictionaryData:
-                memDataDict = dict()
-                for i, val in enumerate(row):
-                    if type(val) == datetime:
-                        val = time.mktime(val.timetuple())
-                    memDataDict[columns[i]] = val
-                memData.append(memDataDict)
-            r.set(query, dumps(memData))
-
-            executionTime = (endTime - startTime) * 1000
-            # totalExecutionTime = totalExecutionTime + executionTime
-        # print(totalExecutionTime)
-    # with cache
-    else:
-        for i in range(noOfQueries):
-            magnitude_value = round(random.uniform(1, 10), 2)
-            query = "SELECT locationSource FROM earthquake_new WHERE mag = '" + str(magnitude_value) + "'"
-            # print("inside else")
-            memhash = hashlib.sha256(query.encode()).hexdigest()
-            startTime = time.time()
-            lstDictionaryData = r.get(memhash)
-
-            # print(lstDictionaryData[0])
-            # print(i)
-            if not lstDictionaryData:
-                # print("from db")
-
-                cursor.execute(query)
-                lstDictionaryData = cursor.fetchall()
-                if i == 0:
-                    # print("from db")
-                    lstDictionaryDataDisplay = lstDictionaryData
-                endTime = time.time()
-                memData = []
-                for row in lstDictionaryData:
-                    memDataDict = dict()
+                formatted_data = []
+                for row in rows:
+                    quake = dict()
                     for i, val in enumerate(row):
                         if type(val) == datetime:
                             val = time.mktime(val.timetuple())
-                        memDataDict[columns[i]] = val
-                    memData.append(memDataDict)
-                r.set(memhash, dumps(memData))
+                        quake[columns[i]] = val
+                    formatted_data.append(quake)
+                redis.set(query_hash, dumps(formatted_data))
+
+                result = loads(redis.get(query_hash)).decode()
+    else:
+        source_used = 'Azure SQL'
+        for mag in random_list:
+            formatted_query = sqlquery.format(columns_str=columns_str, mag=mag)
+            query_hash = hashlib.sha256(formatted_query.encode()).hexdigest()
+            cursor.execute(formatted_query)
+            rows = cursor.fetchall()
+
+            # if rows:
+            #     print('Values present for: ',query_hash)
+            formatted_data = []
+            for row in rows:
+                quake = dict()
+                for i, val in enumerate(row):
+                    if type(val) == datetime:
+                        val = time.mktime(val.timetuple())
+                    quake[columns[i]] = val
+                formatted_data.append(quake)
+            redis.set(query_hash, dumps(formatted_data))
+
+            result = formatted_data
+
+    time_taken = time.time() - t
+
+    return render_template('results.html', time_taken=time_taken, count=cnt, source=source_used)
+
+
+@app.route('/analyze_sameq')
+def analyze_sameq():
+    cnt = int(request.args.get('count', 0))
+    source = request.args.get('source', 'sqldb')
+    s_mag = float(request.args.get('s_mag', 2))
+    e_mag = float(request.args.get('e_mag', 3))
+
+    columns = ['time', 'latitude', 'longitude', 'place', 'mag']
+    columns_str = '"' + '","'.join(columns) + '"'
+
+    sqlquery = 'select {columns_str} from dbo.all_month where mag between {s_mag} and {e_mag};'
+    cursor = database.connection.cursor()
+    t = time.time()
+    if source == 'cache':
+        source_used = 'Redis Cache'
+        for i in range(cnt):
+            formatted_query = sqlquery.format(columns_str=columns_str, s_mag=s_mag, e_mag=e_mag)
+            query_hash = hashlib.sha256(formatted_query.encode()).hexdigest()
+            result = redis.get(query_hash)
+            if result:
+                pass
             else:
-                lstDictionaryData = loads(lstDictionaryData.decode())
-                if i == 0:
-                    lstDictionaryDataDisplay = lstDictionaryData
-                endTime = time.time()
-            executionTime = (endTime - startTime) * 1000
-            if i == 0:
-                firstExecutionTime = executionTime
-            totalExecutionTime = totalExecutionTime + executionTime
-    conn.close()
-    # print(lstDictionaryData)
-    return render_template('index.html', tableData=lstDictionaryDataDisplay, tableDataLen=lstDictionaryDataDisplay.__len__(), executionTime=totalExecutionTime, firstExecutionTime=firstExecutionTime)
+                cursor.execute(formatted_query)
+                rows = cursor.fetchall()
+
+                # if rows:
+                #     print('Values present for: ',query_hash)
+
+                formatted_data = []
+                for row in rows:
+                    quake = dict()
+                    for i, val in enumerate(row):
+                        if type(val) == datetime:
+                            val = time.mktime(val.timetuple())
+                        quake[columns[i]] = val
+                    formatted_data.append(quake)
+                redis.set(query_hash, dumps(formatted_data))
+
+                result = redis.get(query_hash)
+            result = loads(result.decode())
+    else:
+        source_used = 'Azure SQL'
+        for ri in range(cnt):
+            formatted_query = sqlquery.format(columns_str=columns_str, s_mag=s_mag, e_mag=e_mag)
+            query_hash = hashlib.sha256(formatted_query.encode()).hexdigest()
+            # print('QUERY:',formatted_query)
+            cursor.execute(formatted_query)
+            rows = cursor.fetchall()
+
+            # if rows:
+            #     print('Values present for: ',query_hash)
+            formatted_data = []
+            for row in rows:
+                quake = dict()
+                for i, val in enumerate(row):
+                    if type(val) == datetime:
+                        val = time.mktime(val.timetuple())
+                    quake[columns[i]] = val
+                formatted_data.append(quake)
+            redis.set(query_hash, dumps(formatted_data))
+
+            result = formatted_data
+
+    time_taken = time.time() - t
+    print('okay')
+    return render_template('results.html', time_taken=time_taken, count=cnt, source=source_used, earthquakes=result)
 
 
-# @app.route('/upload_  data', methods=['POST'])
-# def upload():
-#     sqlquery = 'Insert into "all_month" ({columns}) values ({values})'
-#     tempfile = request.files['files']
-#     filename = 'tempcsv'
-#     tempfile.save(os.path.join(filename))
-#
-#     csv_file = open(filename, 'r')
-#     reader = csv.DictReader(csv_file)
-#     for row in reader:
-#         cols = '"' + '","'.join(row.keys()) + '"'
-#         vals = '\'' + '\',\''.join(row.values()) + '\''
-#         q = sqlquery.format(columns=cols, values=vals)
-#         print("QUERy:", q)
-#         cursor = database.connection.cursor()
-#         cursor.execute(q)
-#     csv_file.close()
+@app.route('/get_some')
+def get_some():
+    columns = ['time', 'latitude', 'longitude', 'place', 'mag']
+    sqlquery = 'select top 10 {columns} from dbo.all_month;'.format(columns='"' + '","'.join(columns) + '"')
+    cursor = database.connection.cursor()
+    cursor.execute(sqlquery)
+    row = cursor.fetchone()
+    all_data = []
 
-# @app.route('/uploaddata')
-# def uploadData():
-#     return render_template('index.html', tableData=lstDictionaryData, tableDataLen=lstDictionaryData.__len__())
+    while row:
+        quake = dict()
+        for i, val in enumerate(row):
+            quake[columns[i]] = val
+        all_data.append(quake)
+        row = cursor.fetchone()
+    print('QUAKEs:', all_data)
+    return render_template('index.html', earthquakes=all_data)
+
+
+@app.route('/upload_data', methods=['POST'])
+def upload():
+    sqlquery = 'Insert into "all_month" ({columns}) values ({values})'
+    tempfile = request.files['files']
+    filename = 'tempcsv'
+    tempfile.save(os.path.join(filename))
+
+    csv_file = open(filename, 'r')
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        cols = '"' + '","'.join(row.keys()) + '"'
+        vals = '\'' + '\',\''.join(row.values()) + '\''
+        q = sqlquery.format(columns=cols, values=vals)
+        print("QUERy:", q)
+        cursor = database.connection.cursor()
+        cursor.execute(q)
+    csv_file.close()
+    # file.save(os.path.join(Uploadpath, file_name))
+    # con = engine.connect()
+    # df = pd.read_csv(os.path.join(Uploadpath, file_name))
+    # tab_file = file_name.split('.')[0]
+    # start_time = time.time()
+    # df.to_sql(name=tab_file, con=con, if_exists='replace', index=False)
+    # end_time = time.time()
+    # diff = str((end_time - start_time) * 1000)
+    # con.close()
+    return render_template('index.html', earthquakes=[])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
